e731a83efd79af4603dd8657dadc2bda52e83b1a
Mike Ouellette <michael.ouellette@dcsg.com>
2023-02-06 10:11:56 -0500

    Demo work

--- challenges/02-rendering/main.ts
+++ challenges/02-rendering/main.ts
@@ -1,8 +1,32 @@
+import './main.css';
+
 import { LocationCard, Warning } from '../../components';
 
-import './main.css';
 import { Rendering } from './Rendering';
 
 customElements.define('app-location-card', LocationCard);
 new Rendering();
 new Warning();
+
+class Touch {
+	private boundOnTouchMove = this.onTouchMove.bind(this);
+	private readonly images: NodeListOf<HTMLImageElement> | null = null;
+
+	constructor() {
+	  this.images = document.querySelectorAll('img');
+	  this.images.forEach((img) => {
+		img.addEventListener('touchmove', this.boundOnTouchMove);
+	  });
+	}
+
+	private fibonacci(num: number): number {
+		if (num <= 1) return 1;
+		return this.fibonacci(num - 1) + this.fibonacci(num - 2);
+	}
+
+	private onTouchMove(event: TouchEvent): void {
+		this.fibonacci(35);
+    	console.log('touchmove');
+	}
+}
+new Touch();
--- components/ResortCard.ts
+++ components/ResortCard.ts
@@ -1,153 +1,159 @@
 export class ResortCard extends HTMLElement {
-  static get observedAttributes(): string[] {
-    return [
-      'resort-name',
-      'state',
-      'summit',
-      'base',
-      'vertical',
-      'lifts',
-      'runs',
-      'acres',
-      'green-percent',
-      'blue-percent',
-      'black-percent',
-    ];
-  }
+	static get observedAttributes(): string[] {
+		return [
+			'resort-name',
+			'state',
+			'summit',
+			'base',
+			'vertical',
+			'lifts',
+			'runs',
+			'acres',
+			'green-percent',
+			'blue-percent',
+			'black-percent',
+		];
+	}
 
-  /** Boolean indicating if the resort is a favorite. */
-  favorite = false;
+	/** Boolean indicating if the resort is a favorite. */
+	favorite = false;
 
-  /** The button to favorite the resort. */
-  private favoriteButton: HTMLButtonElement | null = null;
+	/** The button to favorite the resort. */
+	private favoriteButton: HTMLButtonElement | null = null;
 
-  /** The favorite outline svg. */
-  private favoriteOutline: SVGElement | null = null;
+	/** The favorite outline svg. */
+	private favoriteOutline: SVGElement | null = null;
 
-  /** The favorite solid svg. */
-  private favoriteSolid: SVGElement | null = null;
+	/** The favorite solid svg. */
+	private favoriteSolid: SVGElement | null = null;
 
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    const template = document.getElementById(
-      'resort-card'
-    ) as HTMLTemplateElement;
-    if (!template) {
-      return;
-    }
-    const templateContent = template.content;
-    this.attachShadow({ mode: 'open' }).appendChild(
-      templateContent.cloneNode(true)
-    );
+		const template = document.getElementById(
+			'resort-card'
+		) as HTMLTemplateElement;
+		if (!template) {
+			return;
+		}
+		const templateContent = template.content;
+		this.attachShadow({ mode: 'open' }).appendChild(
+			templateContent.cloneNode(true)
+		);
 
-    if (this.shadowRoot) {
-      this.favoriteButton = this.shadowRoot.querySelector('#favorite');
-      this.favoriteOutline = this.shadowRoot.querySelector('#favorite-outline');
-      this.favoriteSolid = this.shadowRoot.querySelector('#favorite-solid');
-    }
-    this.onFavorite = this.onFavorite.bind(this);
-  }
+		if (this.shadowRoot) {
+			this.favoriteButton = this.shadowRoot.querySelector('#favorite');
+			this.favoriteOutline = this.shadowRoot.querySelector('#favorite-outline');
+			this.favoriteSolid = this.shadowRoot.querySelector('#favorite-solid');
+		}
+		this.onFavorite = this.onFavorite.bind(this);
+	}
 
-  connectedCallback(): void {
-    if (this.favoriteButton) {
-      this.favoriteButton.addEventListener('click', this.onFavorite);
-    }
-    ['green', 'blue', 'black'].forEach((color) => {
-      const qualifiedName = `${color}-percent`;
-      if (this.getAttribute(qualifiedName) !== null) {
-        this.setAcresPercent(color, +!this.getAttribute(qualifiedName));
-      }
-    });
-  }
+	connectedCallback(): void {
+		if (this.favoriteButton) {
+			this.favoriteButton.addEventListener('click', this.onFavorite);
+		}
+		['green', 'blue', 'black'].forEach((color) => {
+			const qualifiedName = `${color}-percent`;
+			if (this.getAttribute(qualifiedName) !== null) {
+				this.setAcresPercent(color, +!this.getAttribute(qualifiedName));
+			}
+		});
+	}
 
-  attributeChangedCallback(
-    name: string,
-    oldValue: string,
-    newValue: string
-  ): void {
-    switch (name) {
-      case 'green-percent':
-        this.setAcresPercent('green', +newValue);
-        break;
-      case 'blue-percent':
-        this.setAcresPercent('blue', +newValue);
-        break;
-      case 'black-percent':
-        this.setAcresPercent('black', +newValue);
-        break;
-      default:
-        this.setSlotInnerText(name);
-    }
-  }
+	attributeChangedCallback(
+		name: string,
+		oldValue: string,
+		newValue: string
+	): void {
+		switch (name) {
+			case 'green-percent':
+				this.setAcresPercent('green', +newValue);
+				break;
+			case 'blue-percent':
+				this.setAcresPercent('blue', +newValue);
+				break;
+			case 'black-percent':
+				this.setAcresPercent('black', +newValue);
+				break;
+			default:
+				this.setSlotInnerText(name);
+		}
+	}
 
-  onFavorite(): void {
-    if (!this.favoriteButton || !this.favoriteOutline || !this.favoriteSolid) {
-      return;
-    }
-    let styles = '';
-    if (this.favorite) {
-      styles = `
-        #favorite-outline {
-          display: none;
-        }
-        #favorite-solid {
-          display: block;
-        }
-      `;
-    } else {
-      styles = `
-        #favorite-outline {
-          display: none;
-        }
-        #favorite-solid {
-          display: block;
-        }
-      `;
-    }
-    const style = document.createElement('style');
-    style.textContent = styles;
-    if (this.shadowRoot) {
-      this.shadowRoot.appendChild(style);
-    }
-  }
+	onFavorite(): void {
+		if (!this.favoriteButton || !this.favoriteOutline || !this.favoriteSolid) {
+			return;
+		}
+		let styles = '';
+		if (this.favorite) {
+			styles = `
+				#favorite-outline {
+					display: none;
+				}
+				#favorite-solid {
+					display: block;
+				}
+			`;
+		} else {
+			styles = `
+				#favorite-outline {
+					display: none;
+				}
+				#favorite-solid {
+					display: block;
+				}
+			`;
+		}
+		const style = document.createElement('style');
+		style.textContent = styles;
+		if (this.shadowRoot) {
+			this.shadowRoot.appendChild(style);
+		}
+	}
 
-  private animatePercent(el: HTMLDivElement, percent: number, prev = 0): void {
-    if (prev >= percent) {
-      return;
-    }
-    prev++;
-    el.style.width = `${prev.toString()}%`;
-    requestAnimationFrame(() => {
-      el.style.width = `${prev.toString()}%`;
-      this.animatePercent(el, percent, prev);
-    });
-  }
+	private setAcresPercent(color: string, percent: number): void {
+		if (!this.shadowRoot || !percent) {
+			return;
+		}
+		const percentEl = this.shadowRoot.querySelector(
+			`footer > .stat.${color} > .bar > .percent`
+		) as HTMLDivElement;
+		if (!percentEl) {
+			return;
+		}
 
-  private setAcresPercent(color: string, percent: number): void {
-    if (!this.shadowRoot || !percent) {
-      return;
-    }
-    const percentEl = this.shadowRoot.querySelector(
-      `footer > .stat.${color} > .bar > .percent`
-    ) as HTMLDivElement;
-    if (!percentEl) {
-      return;
-    }
-    this.animatePercent(percentEl, percent * 100);
-    // percentEl.style.width = `${(percent * 100).toString()}%`;
-  }
+		const intersectionObserver = new IntersectionObserver((entries) => {
+			if(entries.length === 0) {
+				return;
+			}
+			const entry = entries[0];
+			if (entry.isIntersecting) {
+				requestAnimationFrame(() => {
+					percentEl.style.width = `${(percent * 100).toString()}%`;
+				});
+			} else if (percentEl.getBoundingClientRect().top <= window.innerHeight) {
+				percentEl.style.width = `${(percent * 100).toString()}%`;
+			}
+		},
+		{
+			root: null,
+			threshold: 1.0,
+		});
 
-  private setSlotInnerText(name: string): void {
-    if (!this.shadowRoot) {
-      return;
-    }
-    const slot = this.shadowRoot.querySelector(
-      `slot[name=${name}]`
-    ) as HTMLSlotElement;
-    if (!slot) {
-      return;
-    }
-    slot.textContent = this.getAttribute(name);
-  }
+		intersectionObserver.observe(percentEl);
+	}
+
+	private setSlotInnerText(name: string): void {
+		if (!this.shadowRoot) {
+			return;
+		}
+		const slot = this.shadowRoot.querySelector(
+			`slot[name=${name}]`
+		) as HTMLSlotElement;
+		if (!slot) {
+			return;
+		}
+		slot.textContent = this.getAttribute(name);
+	}
 }
